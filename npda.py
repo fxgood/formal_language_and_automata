fb5e0530c77a932cfce4ef634c789e13f635627d
yfx <1425931231@qq.com>
2020-11-02 18:16:37 +0800

    11

--- npda.py
+++ npda.py
@@ -9,13 +9,12 @@ class Rule:
         self.stack_top_replace_str=stack_top_replace_str    # 栈顶更换的串
 
 class NPDA:
-    state_symbols=['q^0','q^1','q^2']
-    # 'q^3','q^4','q^5','q^6','q^7','q^8','q^9','q^10'
     # 传入一个字典形式的Greibach范式
     def __init__(self,grammar):
-        self.tape=['#','#']
+        self.tape=['#']
+        self.index=0
         self.state='q^0'
-        self.stack=[]   # 用列表模拟一个栈
+        self.stack=['z']   # 用列表模拟一个栈，左边是栈顶
         self.rules=[Rule('q^0','q^1','#','z','Sz'),
                     Rule('q^1','q^2','#','z','z')]
         # 根据greibach范式生成规则
@@ -32,6 +31,41 @@ class NPDA:
     def show_rules(self):
         for e in self.rules:
             print('$('+e.read_state+','+e.read_char+','+e.stack_top+')=('+e.out_state+','+e.stack_top_replace_str+')')
+
+    def __recognize(self,string,read_state,stack):
+        # 递归终止条件，达到终态
+        if read_state=='q^2':
+            return True
+        # 递归终止条件，扫描完了tape还没到达终态
+        if string == '':
+            return False
+        now_string=string
+        now_stack=stack
+        now_read_state=read_state
+        matched_rules=[]
+        for rule in self.rules:
+            if rule.read_state == now_read_state \
+                    and rule.read_char == now_string[0] \
+                    and rule.stack_top == now_stack[0]:
+                matched_rules.append(rule)
+        # 递归终止条件，没到达终态的情况下，没有任何规则可以匹配
+        if not matched_rules:
+            return False
+        if len(matched_rules)==1:
+            pass
+        else:
+            result=False
+            for rule in matched_rules:
+                result=result or self.__recognize(string[1:],now_read_state,now_stack)
+            return result
+
+    def recognize_language(self,string):
+        self.tape += string
+        self.tape.append('#')
+        return self.__recognize(self.tape,'q^0',self.stack)
+
+
+
 
 if __name__=='__main__':
     case_1={
@@ -59,10 +93,13 @@ if __name__=='__main__':
     }
     case_5={
         'S':['aA'],
-        'A':['aABC','bB','a'],
+        'A':['a','aABC','bB'],
         'B':['b'],
         'C':['c']
     }
     p=toGreibach(case_4)
     p.to_greibach()
-    NPDA(case_5).show_rules()
+    npda=NPDA(case_5)
+    npda.show_rules()
+    result=npda.recognize_language('aabb')
+    print(result)
